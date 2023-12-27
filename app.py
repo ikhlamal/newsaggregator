d5ecc129e8effd1ecf0c991ba617bc80a5b1a7c7
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-27 11:28:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -27,15 +27,23 @@ def main():
     thumbnail_url = get_news_thumbnail(entry.link)
 
     # Tampilkan informasi berita dalam layout Streamlit
+    col1, col2 = st.columns([1, 2])
+
+    # Kolom pertama (thumbnail)
     if thumbnail_url:
-        st.image(thumbnail_url, caption="Berita Terkini", use_column_width=True)
+        col1.image(thumbnail_url, caption="", use_column_width=True)
 
-    st.header(entry.title)
-    st.subheader(entry.published)
+    # Kolom kedua (judul, tanggal, dan link)
+    with col2:
+        st.header(entry.title)
+        st.subheader(entry.published)
 
-    st.write(entry.summary)
+        # Tambahkan hyperlink pada judul
+        st.markdown(f"[Baca Berita]({entry.link})")
 
-    st.text("Sumber: " + entry.source.title)
+        st.write(entry.summary)
+
+        st.text("Sumber: " + entry.source.title)
 
 if __name__ == "__main__":
     main()
