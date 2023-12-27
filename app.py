cdcc58ebc476f688b33a611b13bbbad11a194ac0
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-27 12:14:51 +0700

    Update app.py

--- app.py
+++ app.py
@@ -50,11 +50,11 @@ def main():
             col.markdown(
                 f"""
                 <div style="border: 1px solid #ccc; border-radius: 10px; padding: 10px; display: flex; align-items: center; margin-bottom: 10px;">
-                    <img src="{thumbnail_url}" alt="Thumbnail" style="max-width: 100px; height: auto; margin-right: 10px;">
+                    <img src="{thumbnail_url}" alt="Thumbnail" style="max-width: 80px; height: auto; margin-right: 10px;">
                     <div style="flex: 1;">
-                        <h4 style='text-align: left; font-size: 18px; margin-bottom: 5px;'><a href='{entry.link}' target='_blank'>{entry.title}</a></h4>
-                        <p style='text-align: left; font-size: 14px; margin-bottom: 5px;'>{format_time_difference(entry.published)}</p>
-                        <p style='text-align: left; font-size: 14px;'>Sumber: {entry.source.title}</p>
+                        <h4 style='text-align: left; font-size: 16px; margin-bottom: 5px;'><a href='{entry.link}' target='_blank'>{entry.title}</a></h4>
+                        <p style='text-align: left; font-size: 12px; margin-bottom: 5px;'>{format_time_difference(entry.published)}</p>
+                        <p style='text-align: left; font-size: 12px;'>Sumber: {entry.source.title}</p>
                     </div>
                 </div>
                 """,
