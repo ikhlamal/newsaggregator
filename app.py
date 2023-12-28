fd6a34f1523fd38d2ae9c012d0c8d322a85ead97
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 08:41:16 +0700

    Update app.py

--- app.py
+++ app.py
@@ -32,12 +32,11 @@ def main():
     rss_url = 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtbGtHZ0pKUkNnQVAB?hl=id&gl=ID&ceid=ID%3Aid&oc=11'
     feed = feedparser.parse(rss_url)
 
-    # Dropdown untuk memilih berita
-    options = ["Berita Utama", "Berita Terkait 1", "Berita Terkait 2", "Berita Terkait 3"]
-    selected_option = st.expander("Pilih Berita:").selectbox("", options, index=0)
+    # Sidebar untuk dropdown
+    selected_option = st.sidebar.selectbox("Pilih Berita:", ["Berita Utama", "Berita Terkait 1", "Berita Terkait 2", "Berita Terkait 3"])
 
     # Tampilkan berita yang dipilih
-    with st.expander("Berita Utama", expanded=("Berita Utama" in selected_option)):
+    if 'Berita Utama' in selected_option:
         entry = feed.entries[0]
         thumbnail_url = get_news_thumbnail(entry.link)
         if thumbnail_url:
@@ -52,31 +51,29 @@ def main():
                 """,
                 unsafe_allow_html=True
             )
+    elif 'Berita Terkait' in selected_option:
+        # Ambil berita terkait yang dipilih
+        entry = feed.entries[0]
+        summaries = BeautifulSoup(entry.summary, 'html.parser').find_all('a')[1:4]
+        selected_summary = summaries[int(selected_option[-1]) - 1]
+        link = selected_summary.get('href')
+        title = selected_summary.get_text(strip=True)
+        source = selected_summary.find_next('font').get_text(strip=True)
 
-    for i in range(3):
-        with st.expander(f"Berita Terkait {i + 1}", expanded=(f"Berita Terkait {i + 1}" in selected_option)):
-            # Ambil berita terkait yang dipilih
-            entry = feed.entries[0]
-            summaries = BeautifulSoup(entry.summary, 'html.parser').find_all('a')[1:4]
-            selected_summary = summaries[i]
-            link = selected_summary.get('href')
-            title = selected_summary.get_text(strip=True)
-            source = selected_summary.find_next('font').get_text(strip=True)
+        thumbnail_url_related = get_news_thumbnail(link)
 
-            thumbnail_url_related = get_news_thumbnail(link)
-
-            if thumbnail_url_related:
-                st.markdown(
-                    f"""
-                    <div style="border: 1px solid #ccc; border-radius: 10px; padding: 10px; text-align: left; margin-bottom: 10px;">
-                        <img src="{thumbnail_url_related}" alt="Thumbnail" style="max-width: 600px; max-height: 400px; margin-bottom: 10px;">
-                        <h4 style='font-size: 16px; margin-bottom: 5px;'><a href='{link}' target='_blank'>{title}</a></h4>
-                        <p style='font-size: 12px; margin-bottom: 5px;'>x jam yang lalu</p>
-                        <p style='font-size: 12px; margin-bottom: 5px;'>Sumber: {source}</p>
-                    </div>
-                    """,
-                    unsafe_allow_html=True
-                )
+        if thumbnail_url_related:
+            st.markdown(
+                f"""
+                <div style="border: 1px solid #ccc; border-radius: 10px; padding: 10px; text-align: left; margin-bottom: 10px;">
+                    <img src="{thumbnail_url_related}" alt="Thumbnail" style="max-width: 600px; max-height: 400px; margin-bottom: 10px;">
+                    <h4 style='font-size: 16px; margin-bottom: 5px;'><a href='{link}' target='_blank'>{title}</a></h4>
+                    <p style='font-size: 12px; margin-bottom: 5px;'>x jam yang lalu</p>
+                    <p style='font-size: 12px; margin-bottom: 5px;'>Sumber: {source}</p>
+                </div>
+                """,
+                unsafe_allow_html=True
+            )
 
 if __name__ == "__main__":
     main()
