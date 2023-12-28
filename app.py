e6235d6727f1b4fd8a1380152a8177db86703aa3
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 12:27:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,20 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import requests
 
+import requests
+from bs4 import BeautifulSoup
+
 def get_news_thumbnail(url):
-    response = requests.get(url)
-    if response.status_code == 200:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Referer': 'https://www.google.com/',
+    }
+
+    try:
+        response = requests.get(url, headers=headers, timeout=10)
+        response.raise_for_status()  # Raise an HTTPError for bad responses (4xx and 5xx)
+        
         soup = BeautifulSoup(response.text, 'html.parser')
 
         # Coba cari thumbnail menggunakan tag 'meta'
@@ -22,8 +33,8 @@ def get_news_thumbnail(url):
         # Tambahkan tag atau class lain yang sesuai dengan struktur website tertentu
 
         return None
-    else:
-        print(f"Error: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
         return None
         
 def get_news_article(url):
