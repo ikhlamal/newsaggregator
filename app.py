660efbf0c65ed52de515dc35af8c2bb57e562108
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 12:39:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,20 +8,7 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_news_thumbnail(url):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.5',
-        'Accept-Encoding': 'gzip, deflate, br',
-        'Connection': 'keep-alive',
-    }
-
-    try:
-        response = requests.get(url, headers=headers, timeout=10)
-        response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        return None
-
+    response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
 
