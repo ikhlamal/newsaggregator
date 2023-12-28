2c59a8e4be362797f059ff3503b1801a2c70a5f4
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 08:15:15 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,34 +52,29 @@ def main():
     selected_news = st.radio("Pilih Berita:", ["Berita Utama"] + [f"Berita Terkait {i + 1}" for i in range(3)], index=0)
 
     # Kolom 2, 3, dan 4 (berita terkait)
-    if 'summary' in entry:
-        if "Berita Utama" in selected_news:
-            selected_entry = entry
-        else:
-            summaries = BeautifulSoup(entry.summary, 'html.parser').find_all('a')[1:4]  # Ambil 3 berita terkait ke-2 hingga ke-4
+    if 'summary' in entry and "Berita Utama" not in selected_news:
+        summaries = BeautifulSoup(entry.summary, 'html.parser').find_all('a')[1:4]  # Ambil 3 berita terkait ke-2 hingga ke-4
+        try:
             selected_summary = summaries[int(selected_news.split()[-1]) - 1]
-            
-            try:
-                link = selected_summary.get('href')
-                selected_entry = feedparser.parse(link).entries[0]
-            except IndexError:
-                st.error("Tidak dapat mengambil berita terkait. Coba pilih berita utama.")
-                return
+            link = selected_summary.get('href')
+            selected_entry = feedparser.parse(link).entries[0]
 
-        thumbnail_url_related = get_news_thumbnail(selected_entry.link)
+            thumbnail_url_related = get_news_thumbnail(selected_entry.link)
 
-        if thumbnail_url_related:
-            st.markdown(
-                f"""
-                <div style="border: 1px solid #ccc; border-radius: 10px; padding: 10px; text-align: left; margin-bottom: 10px;">
-                    <img src="{thumbnail_url_related}" alt="Thumbnail" style="max-width: 600px; max-height: 400px; margin-bottom: 10px;">
-                    <h4 style='font-size: 16px; margin-bottom: 5px;'><a href='{selected_entry.link}' target='_blank'>{selected_entry.title}</a></h4>
-                    <p style='font-size: 12px; margin-bottom: 5px;'>{format_time_difference(selected_entry.published)}</p>
-                    <p style='font-size: 12px;'>Sumber: {selected_entry.source.title}</p>
-                </div>
-                """,
-                unsafe_allow_html=True
-            )
+            if thumbnail_url_related:
+                st.markdown(
+                    f"""
+                    <div style="border: 1px solid #ccc; border-radius: 10px; padding: 10px; text-align: left; margin-bottom: 10px;">
+                        <img src="{thumbnail_url_related}" alt="Thumbnail" style="max-width: 600px; max-height: 400px; margin-bottom: 10px;">
+                        <h4 style='font-size: 16px; margin-bottom: 5px;'><a href='{selected_entry.link}' target='_blank'>{selected_entry.title}</a></h4>
+                        <p style='font-size: 12px; margin-bottom: 5px;'>{format_time_difference(selected_entry.published)}</p>
+                        <p style='font-size: 12px;'>Sumber: {selected_entry.source.title}</p>
+                    </div>
+                    """,
+                    unsafe_allow_html=True
+                )
+        except IndexError:
+            st.warning("Tidak dapat menemukan berita terkait yang dipilih.")
 
 if __name__ == "__main__":
     main()
