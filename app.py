3c4bf5cec9721715233a5603014a28bf4e13418b
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 14:13:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def main():
     st.set_page_config(layout="wide")
     st.title("Contoh Aja")
 
-    rss_url = 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtbGtHZ0pKUkNnQVAB?hl=id&gl=ID&ceid=ID%3Aid&oc=11'
+    rss_url = 'https://news.google.com/rss/search?hl=id&gl=ID&ceid=ID%3Aid&oc=11&q=kompas'
     feed = feedparser.parse(rss_url)
 
     # Sidebar untuk dropdown
