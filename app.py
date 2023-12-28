3520d3d8d40e440a02f1c18162c7fabc8714f0d8
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 08:27:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def main():
 
     # Dropdown untuk memilih berita
     options = ["Berita Utama", "Berita Terkait 1", "Berita Terkait 2", "Berita Terkait 3"]
-    selected_option = st.selectbox("Pilih Berita:", options, index=0)
+    selected_option = st.selectbox("Pilih Berita:", options, index=-1)
 
     # Tampilkan berita yang dipilih
     if 'Berita Utama' in selected_option:
