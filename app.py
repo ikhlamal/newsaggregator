96939c5d6d7d27edcd763c3ffcee113107fa1b8a
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2023-12-28 12:43:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
     st.set_page_config(layout="wide")
     st.title("Contoh Aja")
 
-    rss_url = 'https://news.google.com/rss/search?hl=id&gl=ID&ceid=ID%3Aid&oc=11&q=cnbc'
+    rss_url = 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRFZxYUdjU0FtbGtHZ0pKUkNnQVAB?hl=id&gl=ID&ceid=ID%3Aid&oc=11'
     feed = feedparser.parse(rss_url)
 
     # Sidebar untuk dropdown
