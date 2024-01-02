1e4145ebf8d367f8ddb33ebf077727631e5b18b3
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-01-02 16:52:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,10 +41,11 @@ def get_news_article(url):
         cnbc = soup.find_all('div', class_='detail_text')
         republika = soup.find_all('div', class_='article-content')
         liputan6 = soup.find_all('div', class_='read-page--content')
-        liputan6 = soup.find_all('div', class_='data-editor-message')
+        tbnews = soup.find_all('div', class_='data-editor-message')
+        kompas = soup.find_all('div', class_='read__content')
 
         # Gabungkan elemen-elemen tersebut
-        all_elements = div_elements + contentx_elements + cnn + detik + cnbc + republika
+        all_elements = div_elements + contentx_elements + cnn + detik + cnbc + republika + liputan6 + tbnews + kompas
 
         # Gabungkan teks dari elemen-elemen yang telah difilter
         article_text = ' '.join(element.get_text() for element in all_elements)
