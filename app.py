94828c4c9e398d96c3ae379b38172f21d01a0031
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 11:14:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,13 +21,7 @@ def main():
     # Menampilkan tweet menggunakan HTML embed code
     st.header("Tweet")
     tweet_html = '''
-    <blockquote class="twitter-tweet" data-media-max-width="560">
-    <p lang="in" dir="ltr">Muncul Aksi dari Caleg Gerindra Protes Guru Besar Unair Kritik Jokowi 
-    <a href="https://t.co/YJMYvUMQrh">https://t.co/YJMYvUMQrh</a>
-    </p>&mdash; CNN Indonesia (@CNNIndonesia) 
-    <a href="https://twitter.com/CNNIndonesia/status/1754454280558485924?ref_src=twsrc%5Etfw">
-    February 5, 2024</a></blockquote>
-    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    <blockquote class="twitter-tweet" data-media-max-width="560"><p lang="in" dir="ltr">sekalian cebokin dah</p>&mdash; PremanTagakSurang (@akunlamakenaTD) <a href="https://twitter.com/akunlamakenaTD/status/1754459937693224989?ref_src=twsrc%5Etfw">February 5, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     '''
     show_tweet(tweet_html)
 
