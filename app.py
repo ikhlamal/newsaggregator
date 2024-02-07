a0d6e6c1076fccbdb41d1841788e4793d6984f78
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 12:20:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,7 @@ def main():
         <blockquote class="twitter-tweet"><p lang="in" dir="ltr">sekalian cebokin dah</p>&mdash; PremanTagakSurang (@akunlamakenaTD) <a href="https://twitter.com/akunlamakenaTD/status/1754459937693224989?ref_src=twsrc%5Etfw">February 5, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         ''',
         '''
-        <blockquote class="twitter-tweet" data-media-max-width="560">
-        <p lang="in" dir="ltr">Tweet 3</p>&mdash; User3 (@user3) 
-        <a href="https://twitter.com/user3/status/3?ref_src=twsrc%5Etfw">
-        January 3, 2024</a></blockquote>
-        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        <blockquote class="twitter-tweet"><p lang="in" dir="ltr">Muncul Aksi dari Caleg Gerindra Protes Guru Besar Unair Kritik Jokowi <a href="https://t.co/YJMYvUMQrh">https://t.co/YJMYvUMQrh</a></p>&mdash; CNN Indonesia (@CNNIndonesia) <a href="https://twitter.com/CNNIndonesia/status/1754454280558485924?ref_src=twsrc%5Etfw">February 5, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         '''
     ]
 
