b9351e754f5f25e48ab745240168991e9646ba1d
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 13:41:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,11 +15,44 @@ def main():
         January 1, 2024</a></blockquote>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
         ''',
-        # Data tweet 2 dan 3...
+        '''
+        <blockquote class="twitter-tweet" data-media-max-width="560">
+        <p lang="in" dir="ltr">Tweet 2</p>&mdash; User2 (@user2) 
+        <a href="https://twitter.com/user2/status/2?ref_src=twsrc%5Etfw">
+        January 2, 2024</a></blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        ''',
+        '''
+        <blockquote class="twitter-tweet" data-media-max-width="560">
+        <p lang="in" dir="ltr">Tweet 3</p>&mdash; User3 (@user3) 
+        <a href="https://twitter.com/user3/status/3?ref_src=twsrc%5Etfw">
+        January 3, 2024</a></blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        '''
     ]
 
     tweets2 = [
-        # Data tweet 1, 2, dan 3...
+        '''
+        <blockquote class="twitter-tweet" data-media-max-width="560">
+        <p lang="in" dir="ltr">Tweet 1</p>&mdash; User1 (@user1) 
+        <a href="https://twitter.com/user1/status/1?ref_src=twsrc%5Etfw">
+        January 1, 2024</a></blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        ''',
+        '''
+        <blockquote class="twitter-tweet" data-media-max-width="560">
+        <p lang="in" dir="ltr">Tweet 2</p>&mdash; User2 (@user2) 
+        <a href="https://twitter.com/user2/status/2?ref_src=twsrc%5Etfw">
+        January 2, 2024</a></blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        ''',
+        '''
+        <blockquote class="twitter-tweet" data-media-max-width="560">
+        <p lang="in" dir="ltr">Tweet 3</p>&mdash; User3 (@user3) 
+        <a href="https://twitter.com/user3/status/3?ref_src=twsrc%5Etfw">
+        January 3, 2024</a></blockquote>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+        '''
     ]
 
     # Menyimpan nilai current_tweet_index di session_state
