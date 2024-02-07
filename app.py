31253a5c37466e47bc5b0d217789db4e6ba97b99
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 22:20:10 +0700

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,8 @@ def main():
                 if st.session_state.current_tweet_index1 > 0:
                     if st.button("⬅️", key="left1"):
                         st.session_state.current_tweet_index1 -= 1
+            with col9:
+                st.write("Next/Prev")
             with col12:
                 if st.session_state.current_tweet_index1 < len(tweets1) - 1:
                     if st.button("➡️", key="right1"):
