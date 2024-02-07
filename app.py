bd82bdb55993abfbc61831779b82aeef7b444cd2
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 11:13:25 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,31 @@ def show_blog_post(thumbnail_url, title, source):
     st.image(thumbnail_url, caption=title, use_column_width=True)
     st.write(f"*Sumber:* {source}")
 
-def show_tweet(tweet_url):
-    st.write(f'<blockquote class="twitter-tweet" data-media-max-width="560"><p lang="in" dir="ltr">Muncul Aksi dari Caleg Gerindra Protes Guru Besar Unair Kritik Jokowi <a href="https://t.co/YJMYvUMQrh">https://t.co/YJMYvUMQrh</a></p>&mdash; CNN Indonesia (@CNNIndonesia) <a href="https://twitter.com/CNNIndonesia/status/1754454280558485924?ref_src=twsrc%5Etfw">February 5, 2024</a></blockquote> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>', unsafe_allow_html=True)
+def show_tweet(tweet_html):
+    st.components.v1.html(tweet_html, width=500, height=400)
 
 def main():
     st.title("Aplikasi Blog dan Tweet Viewer")
 
     # Menampilkan konten blog
     st.header("Konten Blog")
-    thumbnail_url = "https://pluang-production-financial-content-input.s3.ap-southeast-1.amazonaws.com/production/2023/07/lk6kaj4tyleh4wucwar%3Aopenai.jpg"
+    thumbnail_url = "https://example.com/thumbnail.jpg"
     title = "Judul Postingan Blog"
-    source = "ChatGPT"
+    source = "www.exampleblog.com"
     show_blog_post(thumbnail_url, title, source)
 
-    # Menampilkan tweet menggunakan iframe
+    # Menampilkan tweet menggunakan HTML embed code
     st.header("Tweet")
-    tweet_url = "https://twitter.com/CNNIndonesia/status/1754454280558485924"
-    show_tweet(tweet_url)
+    tweet_html = '''
+    <blockquote class="twitter-tweet" data-media-max-width="560">
+    <p lang="in" dir="ltr">Muncul Aksi dari Caleg Gerindra Protes Guru Besar Unair Kritik Jokowi 
+    <a href="https://t.co/YJMYvUMQrh">https://t.co/YJMYvUMQrh</a>
+    </p>&mdash; CNN Indonesia (@CNNIndonesia) 
+    <a href="https://twitter.com/CNNIndonesia/status/1754454280558485924?ref_src=twsrc%5Etfw">
+    February 5, 2024</a></blockquote>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    '''
+    show_tweet(tweet_html)
 
 if __name__ == "__main__":
     main()
