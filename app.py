9e5affb8077ca17fb07391f8e9e8a5325f1712a0
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 12:14:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def show_tweet(tweet_html):
-    st.components.v1.html(tweet_html, width=500, height=1400)
+    st.components.v1.html(tweet_html, width=300, height=700)
 
 def main():
     st.title("Aplikasi Tweet")
