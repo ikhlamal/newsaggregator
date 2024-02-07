532cb012ccee46e888a34917f1dcc49e5e3300b8
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-07 21:42:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -42,12 +42,12 @@ def main():
     col1, col2, col3, col4 = st.columns(4)
 
     with col1:
-        col7, col8 = st.columns(2)
+        col7, col8, col9 = st.columns(3)
         with col7:
             if st.session_state.current_tweet_index1 > 0:
                 if st.button("⬅️", key="left1"):
                     st.session_state.current_tweet_index1 -= 1
-        with col8:
+        with col9:
             if st.session_state.current_tweet_index1 < len(tweets1) - 1:
                 if st.button("➡️", key="right1"):
                     st.session_state.current_tweet_index1 += 1
