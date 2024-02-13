4f9580633f6c94827d835eaf41dbc16cf25cbaca
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-14 05:37:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,21 @@ def main():
                                 else:
                                     st.button("⬅️", key=f"left{index+1}")
                             with col9:
-                                st.text("Mendukung", 5)
+                                ukuran_font = 2  # Misalnya, ukuran font 24px
+
+                                # Gunakan st.markdown() untuk menambahkan tag <style> CSS
+                                st.markdown(
+                                    f"""
+                                    <style>
+                                        /* Atur ukuran font untuk body */
+                                        body {{
+                                            font-size: {ukuran_font}px;
+                                        }}
+                                    </style>
+                                    """,
+                                    unsafe_allow_html=True
+                                )
+                                st.write("Mendukung")
                             with col11:
                                 if st.session_state[f'current_tweet_index{index+1}'] < len(tweets) - 1:
                                     if st.button("➡️", key=f"right{index+1}"):
