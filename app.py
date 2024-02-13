252404f8b9395c770dabc92bbb09ba086aa137c9
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-14 05:32:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def main():
                                     st.button("⬅️", key=f"left{index+1}")
                             with col9:
                                 st.write("Test")
-                            with col9:
+                            with col11:
                                 if st.session_state[f'current_tweet_index{index+1}'] < len(tweets) - 1:
                                     if st.button("➡️", key=f"right{index+1}"):
                                         st.session_state[f'current_tweet_index{index+1}'] += 1
