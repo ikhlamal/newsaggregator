11eb153816b741cbb08d109011baa6b977b07214
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-14 06:35:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -60,9 +60,9 @@ def main():
                                     st.button("⬅️", key=f"left{index+1}")
                             with col9:
                                 if index == 0:
-                                    st.write("🙂🙂: ", len(df))
+                                    st.write("🙂: ", len(df))
                                 elif index == 1:
-                                    st.write("😡😡: ", len(df))      
+                                    st.write("😡: ", len(df))      
                             with col11:
                                 if st.session_state[f'current_tweet_index{index+1}'] < len(tweets) - 1:
                                     if st.button("➡️", key=f"right{index+1}"):
