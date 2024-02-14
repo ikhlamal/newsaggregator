be7adcc448e93f0a00ef6850c9eb52bcee7ef250
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-14 07:18:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -51,13 +51,14 @@ def main():
                         tweets = [format_tweet(row) for index, row in df.iterrows()]
     
                         with st.container(height=650, border=True):
-                            col7, _, col9 = st.columns([1] * 3)
+                            col7, col8, col9 = st.columns([1] * 3)
                             with col7:
                                 if st.session_state[f'current_tweet_index{index+1}'] > 0:
                                     if st.button("⬅️", key=f"left{index+1}"):
                                         st.session_state[f'current_tweet_index{index+1}'] -= 1
                                 else:
-                                    st.button("⬅️", key=f"left{index+1}")     
+                                    st.button("⬅️", key=f"left{index+1}")
+                            with col8:
                                 if st.session_state[f'current_tweet_index{index+1}'] < len(tweets) - 1:
                                     if st.button("➡️", key=f"right{index+1}"):
                                         st.session_state[f'current_tweet_index{index+1}'] += 1
