74ec711b52acf3cb0d50bd442ceb1ce976a5b703
ikhlamal <140026061+ikhlamal@users.noreply.github.com>
2024-02-16 06:12:48 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,80 +1,3 @@
-# import streamlit as st
-# import pandas as pd
-# import os
-# import urllib.parse
-
-
-# def show_tweet(tweet_html):
-#     st.components.v1.html(tweet_html, width=300, height=550, scrolling=True)
-
-# def format_tweet(row):
-#     tweet_html = f'''
-#         <blockquote class="twitter-tweet" data-media-max-width="300">
-#         <a href={row['tweet_url']}></a></blockquote>
-#         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-#     '''
-#     return tweet_html
-
-# def main():
-#     st.set_page_config(layout="centered")
-#     csv_files = ["csv1.csv", "csv2.csv"]
-    
-#     # Filter CSV files that exist
-#     existing_csv_files = [csv_file for csv_file in csv_files if os.path.exists(csv_file)]
-
-#     if len(existing_csv_files) == 0:
-#         st.error("Tidak ada file CSV yang ditemukan.")
-#         return
-
-#     # Menyimpan nilai current_tweet_index di session_state
-#     for i in range(len(existing_csv_files)):
-#         key = f'current_tweet_index{i+1}'
-#         if key not in st.session_state:
-#             st.session_state[key] = 0
-#     with st.expander("***:red[Populer]*** \u2014 Pendukung Minta Prabowo dan Titiek Soeharto Rujuk, Ekspresi Didit Disorot", expanded=False):
-        # num_cols = min(len(existing_csv_files), 2)
-    
-        # # Membagi layar menjadi dua baris dua kolom
-        # for i in range(0, len(existing_csv_files), num_cols):
-        #     cols = st.columns(num_cols)
-        #     for j in range(num_cols):
-        #         index = i + j
-        #         if index < len(existing_csv_files):
-        #             with cols[j]:
-        #                 csv_file = existing_csv_files[index]
-#                         df = pd.read_csv(csv_file)
-#                         if df.empty:
-#                             st.error(f"Tweet dari berita ke-{index+1} kosong.")
-#                             continue
-    
-#                         # List tweet
-#                         tweets = [format_tweet(row) for index, row in df.iterrows()]
-    
-#                         with st.container(height=650, border=True):
-#                             col7, col8, col9 = st.columns([1] * 3)
-#                             with col7:
-#                                 if st.session_state[f'current_tweet_index{index+1}'] > 0:
-#                                     if st.button("⬅️", key=f"left{index+1}"):
-#                                         st.session_state[f'current_tweet_index{index+1}'] -= 1
-#                                 else:
-#                                     st.button("⬅️", key=f"left{index+1}")
-#                             with col8:
-#                                 if st.session_state[f'current_tweet_index{index+1}'] < len(tweets) - 1:
-#                                     if st.button("➡️", key=f"right{index+1}"):
-#                                         st.session_state[f'current_tweet_index{index+1}'] += 1
-#                                 else:
-#                                      st.button("➡️", key=f"right{index+1}")
-#                             with col9:
-#                                 if index == 0:
-#                                     st.write("🙂:", len(df))
-#                                 elif index == 1:
-#                                     st.write("😡:", len(df)) 
-#                             # Menampilkan tweet yang baru setelah klik tombol
-#                             show_tweet(tweets[st.session_state[f'current_tweet_index{index+1}']])
-
-# if __name__ == "__main__":
-#     main()
-
 import streamlit as st
 import pandas as pd
 import os
@@ -93,7 +16,8 @@ def format_tweet(row):
 
 def main():
     st.set_page_config(layout="centered")
-    csv_files = ["csv1.csv", "csv2.csv", "csv3.csv", "csv4.csv", "csv5.csv", "csv6.csv"]
+    csv_files = ["csv1.csv", "csv2.csv", "csv3.csv", "csv4.csv", "csv5.csv", "csv6.csv",
+                "csv7.csv", "csv8.csv"]
     
     # Filter CSV files that exist
     existing_csv_files = [csv_file for csv_file in csv_files if os.path.exists(csv_file)]
@@ -113,7 +37,8 @@ def main():
     titles_and_counts = [
         ("***:red[Populer]*** \u2014 Pendukung Minta Prabowo dan Titiek Soeharto Rujuk, Ekspresi Didit Disorot (Viva)", ["csv1.csv", "csv2.csv"]),
         ("Jokowi Respons Kabar Rencana Bertemu Megawati Dijembatani Sultan DIY (CNN)", ["csv3.csv", "csv4.csv"]),
-        ("Baju Hitam Menteri Jokowi dan Ahok di Hari Pencoblosan Pemilu 2024 (CNN)", ["csv5.csv", "csv6.csv"]) # Ganti judul dan nama file sesuai dengan kebutuhan Anda
+        ("Baju Hitam Menteri Jokowi dan Ahok di Hari Pencoblosan Pemilu 2024 (CNN)", ["csv5.csv", "csv6.csv"]),
+        ("Jokowi Minta Dugaan Kecurangan Pemilu Dilaporkan, PDI-P: Banyak Pihak Justru Ragukan Independensi Bawaslu (Kompas)", ["csv7.csv", "csv8.csv"])# Ganti judul dan nama file sesuai dengan kebutuhan Anda
     ]
 
     for title, csv_pair in titles_and_counts:
